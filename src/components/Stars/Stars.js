0d1c3bcdfb16f82b43be1b69512a0616ac4fa8be
DomenicoCentrone <domenicocentrone22@gmail.com>
2021-11-11 16:18:23 +0100

    .

--- src/components/Stars/Stars.js
+++ src/components/Stars/Stars.js
@@ -1,8 +1,11 @@
 const Stars = () => {
     return (
 
-              <div className="star"></div>
-
+        <div className="wrapper">
+                <div id="stars"></div>
+                <div id="stars2"></div>
+                <div id="stars3"></div>
+        </div>
     );
 };
 
