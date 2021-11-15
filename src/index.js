dc864f4cc9328185675725aa7316cac9660a3d62
Ettore-G <gentile.ettore@hotmail.it>
2021-11-15 11:39:06 +0100

    .

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Router} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import MyRoutes from './routers/routes';
 
 ReactDOM.render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <MyRoutes />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
