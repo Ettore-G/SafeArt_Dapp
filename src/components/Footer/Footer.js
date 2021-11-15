c1bd9695f5ec6e4bc49eb8c1dffd40d327160b0e
Ettore-G <gentile.ettore@hotmail.it>
2021-11-15 17:08:39 +0100

    route fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
+
 
 const BASE_URL = "https://my-json-server.typicode.com/themeland/netstorm-json-2/footer";
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+
+
 const Header = () => {
     return (
         <header id="header">
@@ -7,35 +10,35 @@ const Header = () => {
             <nav data-aos="zoom-out" data-aos-delay={800} className="navbar navbar-expand">
                 <div className="container header">
                     {/* Navbar Brand*/}
-                    <a className="navbar-brand" href="/">
+                    <Link className="navbar-brand" to="/">
                         <img className="navbar-brand-sticky" src="img/pp-removebg-preview.png" alt="sticky brand-logo" />
-                    </a>
+                    </Link>
                     <div className="ml-auto" />
                     {/* Navbar */}
                     <ul className="navbar-nav items mx-auto">
                         <li className="nav-item dropdown">
-                            <a className="nav-link" href="/">Home</a>
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link" href="/">NFT <i className="fas fa-angle-down ml-1" /></a>
+                            <Link className="nav-link" to="/">NFT <i className="fas fa-angle-down ml-1" /></Link>
                             <ul className="dropdown-menu">
-                                <li className="nav-item"><a href="/mint-your-nft" className="nav-link">SafeArt</a></li>
+                                <li className="nav-item"><Link to="/mint-your-nft" className="nav-link">SafeArt</Link></li>
                             </ul>
                         </li>
                         <li className="nav-item">
-                            <a href="/roadmap" className="nav-link">Roadmap</a>
+                            <Link to="/roadmap" className="nav-link">Roadmap</Link>
                         </li>
                         <li className="nav-item">
-                            <a href="https://docs.google.com/document/d/1HJR1H3ASf_thx_BCjg3hNT893oDRlO9H/edit?usp=sharing&ouid=108796879882614269772&rtpof=true&sd=true" target="_blank" className="nav-link">Whitepaper</a>
+                            <Link to="https://docs.google.com/document/d/1HJR1H3ASf_thx_BCjg3hNT893oDRlO9H/edit?usp=sharing&ouid=108796879882614269772&rtpof=true&sd=true" target="_blank" className="nav-link">Whitepaper</Link>
                         </li>
-                            <li className="nav-item"><a href="#staff" className="nav-link">Staff</a></li>
+                            <li className="nav-item"><Link to="#staff" className="nav-link">Staff</Link></li>
                     </ul>
                     {/* Navbar Toggler */}
                     <ul className="navbar-nav toggle">
                         <li className="nav-item">
-                            <a href="/" className="nav-link" data-toggle="modal" data-target="#menu">
+                            <Link to="/" className="nav-link" data-toggle="modal" data-target="#menu">
                                 <i className="fas fa-bars toggle-icon m-0" />
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 
