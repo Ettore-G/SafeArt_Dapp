c3002ba99762fadc7dd983bb99821b999713fc20
Ettore-G <gentile.ettore@hotmail.it>
2021-11-16 17:25:31 +0100

    ..

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <header id="header">
             {/* Navbar */}
-            <nav data-aos="zoom-out" data-aos-delay={800} className="navbar navbar-expand">
+            <nav className="navbar navbar-expand">
                 <div className="container header">
                     {/* Navbar Brand*/}
                     <Link className="navbar-brand" to="/">
@@ -16,11 +16,11 @@ const Header = () => {
                     <div className="ml-auto" />
                     {/* Navbar */}
                     <ul className="navbar-nav items mx-auto">
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link" to="/">Home</Link>
+                        <li className="nav-item ">
+                        <Link to="/" className="nav-link" >Home</Link>
                         </li>
                         <li className="nav-item dropdown">
-                            <Link className="nav-link" to="/">NFT <i className="fas fa-angle-down ml-1" /></Link>
+                            <a className="nav-link">NFT <i className="fas fa-angle-down ml-1" /></a>
                             <ul className="dropdown-menu">
                                 <li className="nav-item"><Link to="/mint-your-nft" className="nav-link">SafeArt</Link></li>
                             </ul>
