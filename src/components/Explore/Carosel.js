58f6cc78c3e606d9721c2aa752e97e108860e760
Ettore-G <gentile.ettore@hotmail.it>
2021-12-16 11:46:53 +0100

    img

--- src/components/Explore/Carosel.js
+++ src/components/Explore/Carosel.js
@@ -5,17 +5,15 @@ class ExploreOne extends Component {
     render() {
         return (
             <section className="live-auctions-area">
-
-
                 <div className="slider-centrale">
-                <figure class="icon-cards mt-3">
-                    <div class="icon-cards__content">
-                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://www.safeart.io/public\img\Octopus\1.png"></img></div>
-                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="\public\img\Octopus\3.png"></img></div>
-                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://picsum.photos/600/602"></img></div>
-                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="daVinci.jpg"></img></div>
-                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://picsum.photos/602/600"></img></div>
-                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://picsum.photos/603/600"></img></div>
+                <figure className="icon-cards mt-3">
+                    <div className="icon-cards__content">
+                        <div className="icon-cards__item d-flex align-items-center justify-content-center octopus-1"></div>
+                        <div className="icon-cards__item d-flex align-items-center justify-content-center octopus-2"></div>
+                        <div className="icon-cards__item d-flex align-items-center justify-content-center octopus-3"></div>
+                        <div className="icon-cards__item d-flex align-items-center justify-content-center octopus-4"></div>
+                        <div className="icon-cards__item d-flex align-items-center justify-content-center octopus-5"></div>
+                        <div className="icon-cards__item d-flex align-items-center justify-content-center octopus-6"></div>
                     </div>
                 </figure>
                 </div>
