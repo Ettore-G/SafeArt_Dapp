f2b53640efb350709cc929e724e2d837cad935cf
DomenicoCentrone <domenicocentrone22@gmail.com>
2021-12-16 10:55:28 +0100

    .

--- src/components/Explore/Carosel.js
+++ src/components/Explore/Carosel.js
@@ -10,10 +10,10 @@ class ExploreOne extends Component {
                 <div className="slider-centrale">
                 <figure class="icon-cards mt-3">
                     <div class="icon-cards__content">
-                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://picsum.photos/600/600"></img></div>
-                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://picsum.photos/600/601"></img></div>
+                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://www.safeart.io/public\img\Octopus\1.png"></img></div>
+                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="\public\img\Octopus\3.png"></img></div>
                         <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://picsum.photos/600/602"></img></div>
-                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://picsum.photos/601/600"></img></div>
+                        <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="daVinci.jpg"></img></div>
                         <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://picsum.photos/602/600"></img></div>
                         <div class="icon-cards__item d-flex align-items-center justify-content-center"><img className="caroselEasy" src="https://picsum.photos/603/600"></img></div>
                     </div>
